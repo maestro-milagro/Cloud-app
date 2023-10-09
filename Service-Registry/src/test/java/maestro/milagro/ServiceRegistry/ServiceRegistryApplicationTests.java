ddbb3892d6f5eba628ee49fbc471418870e72b0c
Sergey Korchagin <millagros77@gmail.com>
2023-10-09 16:54:38 +0300

    Init

---
+++ Service-Registry/src/test/java/maestro/milagro/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package maestro.milagro.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
